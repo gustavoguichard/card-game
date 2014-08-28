32896eaa8bb2ca61e43d4ab39a8ed6a039e84e03
Gustavo Guichard <gustavoguichard@gmail.com>
2014-08-28 18:55:17 -0300

    Fixes bug when game wasn't played

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -8,13 +8,15 @@ class Evaluation < ActiveRecord::Base
 
   def results_for(pile, cards)
     results = {}
-    starred_cards = data.select{|card| card['pile'] == pile and card['starred'] == true}
-    cards.group_by{|c| c[:action]}.each do |group|
-      ids = group[1].map{|oc| oc[:id]}
-      starred_ids = starred_cards.map{|sc| sc['id'] if ids.include?(sc['id'])}
-      action_cards = cards.select{|ac| starred_ids.include?(ac[:id])}
-      unless action_cards.empty?
-        results[group[0]] = action_cards
+    if data
+      starred_cards = data.select{|card| card['pile'] == pile and card['starred'] == true}
+      cards.group_by{|c| c[:action]}.each do |group|
+        ids = group[1].map{|oc| oc[:id]}
+        starred_ids = starred_cards.map{|sc| sc['id'] if ids.include?(sc['id'])}
+        action_cards = cards.select{|ac| starred_ids.include?(ac[:id])}
+        unless action_cards.empty?
+          results[group[0]] = action_cards
+        end
       end
     end
     results
