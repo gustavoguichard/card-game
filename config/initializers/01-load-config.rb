16fe31c82ab141d654b0d179a6875c1dd341f32c
Gustavo Guichard <gustavoguichard@gmail.com>
2014-08-28 22:06:31 -0300

    Changed useless stuff

--- config/initializers/01-load-config.rb
+++ config/initializers/01-load-config.rb
@@ -2,6 +2,4 @@ unless Rails.env.production?
   CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   ENV['ADMIN_USER'] = CONFIG['ADMIN_USER']
   ENV['ADMIN_PASSWORD'] = CONFIG['ADMIN_PASSWORD']
-  ENV['GMAIL_USERNAME'] = CONFIG['GMAIL_USERNAME']
-  ENV['GMAIL_PASSWORD'] = CONFIG['GMAIL_PASSWORD']
 end
