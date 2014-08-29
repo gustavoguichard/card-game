58e3ebeaedc51f4fb4f32fe9e210a1a1014db54e
Gustavo Guichard <gustavoguichard@gmail.com>
2014-08-29 18:34:16 -0300

    Setting Filename for CSV file

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -7,7 +7,7 @@ class EvaluationsController < ApplicationController
     @evaluations = Evaluation.paginate(page: params[:page], per_page: 30)
     respond_to do |format|
       format.html
-      format.csv { send_data @evaluations.to_csv(@cards) }
+      format.csv { send_data @evaluations.to_csv(@cards), filename: 'MI_Game_Results.csv' }
     end
   end
 
