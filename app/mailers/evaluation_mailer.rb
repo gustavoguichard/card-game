ac87e4d8dec373c329e58b97461c4e3532c1489d
Gustavo Guichard <gustavoguichard@gmail.com>
2014-10-16 00:22:29 -0300

    Changed mail sender

--- app/mailers/evaluation_mailer.rb
+++ app/mailers/evaluation_mailer.rb
@@ -1,5 +1,5 @@
 class EvaluationMailer < ActionMailer::Base
-  default from: "gustavoguichard@gmail.com"
+  default from: "noreply.monitorinstitute@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
