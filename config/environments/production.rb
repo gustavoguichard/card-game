6e8ae4e9220ca0591cfab548dfe5cb674b2175ba
Gustavo Guichard <gustavoguichard@gmail.com>
2014-10-16 00:47:40 -0300

    Changed SMTP domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ DeloitteGame::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "monitorinstitute-game.herokuapp.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
